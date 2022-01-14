09c929b8afe62638b77d4c846c8b8f776a4dda04
oaswap <oaswap@protonmail.com>
2022-01-14 14:57:58 -0600

    Init repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# Oaswap Router
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`yarn`
+
+## Compile Contracts
+
+`yarn compile`
+
+## Run Tests
+
+`yarn test`
---
+++ deploy/OaswapRouter.js
@@ -0,0 +1,31 @@
+const { WNATIVE_ADDRESS } = require("@sushiswap/core-sdk");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  const chainId = await getChainId();
+
+  let wethAddress;
+
+  if (chainId === "31337") {
+    wethAddress = (await deployments.get("WETH9Mock")).address;
+  } else if (chainId in WNATIVE_ADDRESS) {
+    wethAddress = WNATIVE_ADDRESS[chainId];
+  } else {
+    throw Error("No WNATIVE!");
+  }
+
+  // const factoryAddress = (await deployments.get("UniswapV2Factory")).address;
+  const factoryAddress = "";
+
+  await deploy("OaswapRouter", {
+    from: deployer,
+    args: [factoryAddress, wethAddress],
+    log: true,
+    deterministicDeployment: false,
+  });
+};
+
+module.exports.tags = ["OaswapRouter", "AMM"];
